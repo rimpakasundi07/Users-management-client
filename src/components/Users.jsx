9ede08c54b7718d35d20f7f7bdfdfec4b9aac037
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-13 20:51:53 +0530

    Add state management and form reset for user creation

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,24 +1,30 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 
 const Users = ({ UsersPromise }) => {
-  const users = use(UsersPromise);
-  console.log(users);
+  const initialUsers = use(UsersPromise);
+  const [users, setUsers] = useState(initialUsers);
 
   const handleAddUser = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     console.log(name, email);
+
+    const newUser = { name, email };
     // send data to the server
     fetch("http://localhost:3000/users", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
+      body: JSON.stringify(newUser),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log("after post", data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        e.target.reset();
       });
   };
 
@@ -51,5 +57,9 @@ export default Users;
  *
  *  Have to send request object to the server
  *  1. Maintion method : post
- *  2. Maintion header : about json data
+ *  2. Maintion header : about json data in the property of content-type : application/json
+ *  3. Body : JSON.stringify(newUser)
+ * ---------------------------------
+ * on the server side use json as middleware
+ * app.use(express.json())
  */
