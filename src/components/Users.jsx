7c5009db1d068ebd38c39d9b70d4b2faf85d9389
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-13 18:40:36 +0530

    Add user form to Users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,6 +6,16 @@ const Users = ({ UsersPromise }) => {
   return (
     <div>
       <div className="">
+        <h3>Add a user</h3>
+        <form>
+          <input type="text" />
+          <br></br>
+          <input type="email" name="" id="" />
+          <br></br>
+          <button> Add user </button>
+        </form>
+      </div>
+      <div className="">
         {users.map((user) => (
           <p>
             Name: {user.name} , Email: {user.email}
